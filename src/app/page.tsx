b30db6f140ebb4c8b709443a360b7df5e334c64c
Mehmet-Kucak <kucakmehmet2019@gmail.com>
2025-07-15 18:33:23 +0300

    Added photo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <>
+      <div className="flex w-1/10 aspect-auto">
+        <Image src="/photo.webp" alt="Photo 2" width={4480} height={6720} />
+      </div>
+    </>
+  );
 }
